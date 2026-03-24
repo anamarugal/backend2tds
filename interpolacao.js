58814a54d21a58e8f5617bda045488fbf35d262e
Ana Marugal <ana.marugal@escola.pr.gov.br>
2026-03-24 14:40:02 +0000

    versao1

---
+++ interpolacao.js
@@ -0,0 +1,11 @@
+const nome = "Ana";
+const idade = 21;
+const anoAtual = 2026;
+const anoNascimento = anoAtual - idade;
+const idadeEmDezAnos = idade + 10;
+
+// Acima só as variavéis.
+
+console.log("O ano de nascimento é: " + anoNascimento);
+console.log(`${nome} possui ${idade} anos.`);
+console.log(`Daqui a 10 anos, terá ${idadeEmDezAnos} anos.`);
