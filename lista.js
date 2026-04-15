948deaaf4d131aad56e82ae3e766c997c749b099
Ana Marugal <ana.marugal@escola.pr.gov.br>
2026-04-15 13:43:40 +0000

    v1

---
+++ lista.js
@@ -0,0 +1,12 @@
+const alunos = ["Ana" , "Pablo", "Brayandy"];
+
+alunos.push("Mateus");
+alunos.unshift("Cauã");
+console.log(alunos);
+
+console.log(alunos.indexOf("Mateus")); //Retorna o indice do array
+console.log(alunos.indexOf("Karla"));
+
+console.log(alunos.includes("Karla"));
+console.log(alunos.includes("Brayandy"));
+
