bdb2bf329405b83232f5cb129de3187ed9a893ae
Ana Marugal <ana.marugal@escola.pr.gov.br>
2026-05-04 13:53:46 +0000

    v3

--- sgp.js
+++ sgp.js
@@ -26,11 +26,26 @@ function filtrarMissoesConcluidas(listaDeMissoes) {
         if (acao === "pegar") {
             const inventarioComItemNovo = [inventarioAtual, nomeDoItem];
             novoInventario = inventarioComItemNovo;
-         } else if (acao === "descartar") //Paramos aqui 
+         } else if (acao === "descartar") {
+            const inventarioSemUltimoItem = inventarioAtual.slice();
+            novoInventario = inventarioSemUltimoItem;
+        } else {
+            novoInventario = inventarioAtual;
+        }
+        return novoInventario; // Retornar a mochila do jogador atualizada 
     };
 
-    const pontosDoJogador = [100, 50, 200, 10]
-    console.log(pontosDoJogador);
+    const pontosDoJogador = [100, 50, 250, 10]
+    const total = calcularPontuacaoTotal(pontosDoJogador);
+    console.log("Pontuação final: ", total);
 
+    const missoes = [
+        {nome: "Salvar a aldeia", status: "Concluída"},
+        {nome: "Encontrar mapa", status: "Em andamento"},
+        {nome: "Derrotar o chefão", status: "Falhou"},
+        {nome: "Coletar recursos", status: "Concluída"} 
+    ];
+
+    console.log(missoes);
 
 
