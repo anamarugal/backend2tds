22e6c5d89b0a7267de9464113650c69c152fd305
Ana Marugal <ana.marugal@escola.pr.gov.br>
2026-05-13 13:54:18 +0000

    v1

---
+++ desafio.js
@@ -0,0 +1,21 @@
+// Parte 1
+// Declarar 5 variaveis para representar um produto
+// preço, nome do produto, numeração de lote, validade do produto, quantidade de estoque 
+
+const nomeDoProduto = "Limpol";
+let preco = 12;
+let numeracaoDoLote = 13579;
+let validadeDoProduto = "01.07.2027";
+let quantidadeEstoque = 20;
+
+console.log(`O produto ${nomeDoProduto}, do lote ${numeracaoDoLote}, validade ${validadeDoProduto} está saindo por ${preco} reais! Corra já, somente ${quantidadeEstoque} unidades no estoque! `);
+
+
+// Parte 2 
+// Crie duas variaveis numericas e exiba uma subtração
+
+let precoInicial = 12;
+const desconto = 0.3;
+const precoComDesconto = precoInicial - desconto;
+
+console.log(``)
